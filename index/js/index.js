8fab036d1270163b80faf61d1a6294229e699894
A0000000000 <mengxin_zzl@outlook.com>
2020-11-04 16:23:35 +0800

    首页布局

---
+++ index/js/index.js
@@ -0,0 +1,14 @@
+$(function () {
+    window.vm = new Vue({
+        el: '#app',
+        data() {
+            return {
+                time: new Date(),
+                visitCount: 0
+            };
+        },
+        methods: {
+
+        }
+    });
+});
